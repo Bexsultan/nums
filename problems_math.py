0b296254ba3f43f18a1cb75d5d864c0363de49e6
Bexsultan <isakov.beksultan2004@gmail.com>
2022-03-15 20:03:59 +0600

    first commit

---
+++ problems_math.py
@@ -0,0 +1,93 @@
+#problem1
+numbers = 17925
+a = (34**2)
+b = (26*3)
+c = (17*33)
+d = (4394*4)
+if numbers <= a:
+	print(a)
+if numbers <= b:
+	print(b)
+if numbers <= c:
+	print(c)
+if numbers <= d:
+	print(d)
+
+#problem2
+x = 5
+y = 4
+z = 7
+abc = y<x<z
+d1 = 7%3 * 4.8
+r1 = ((a) == (b+1))
+n1 = ((2+5) != (3+3))
+print(abc,d1,r1,n1)
+
+#problem3
+l =  (12+28)
+s = (l*2)
+v = (s**2)
+q = (v-193432)
+print(q)
+
+#problem4
+a = (17*3)
+b = (12*5)
+c = (12**3)
+d = (13*7)
+e = (4**5)
+f = (512+512)
+print(c)
+
+#problem5
+a=-21
+b=10
+print(a//b,'Значение к меньшому')
+
+#problem6
+a=2022
+b=83
+c=(a/b)*100
+print(c)
+
+#problem7
+a=2004
+b=2022
+c=(b-a)
+d=(c+2)
+e=(c-2)
+print(d,e)
+
+#problem8
+a=25
+b=75
+c=10
+d=95
+e=(a+b+c+f)
+f=(e/4)
+print(f)
+
+#problem9
+a=5.0
+b=5.0
+c=5.0
+d=(a+b)
+e=(d*c)
+print(e)
+
+#problem10
+a=2
+b=12
+c=2.5
+d=3
+e=7
+
+#problem11
+a=2
+b=12
+c=2.5
+d=3
+e=7
+f=(a-e**(b/d))%c
+print(f)
+
